1bc6777bae20cd413cd284e5e9a9c36df19632ff
haakohu <Haak-hu@hotmail.com>
2016-03-09 15:01:48 +0100

    Endret controllere

--- src/ux/IndexController.java
+++ src/ux/IndexController.java
@@ -1,13 +1,55 @@
 package ux;
 
-import Backend.DBConnect;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IndexController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    private Stage prevStage;
+
+    public Stage getPrevStage() {
+        return prevStage;
+    }
+
+    public void setPrevStage(Stage prevStage) {
+        this.prevStage = prevStage;
+    }
 
 
-public class IndexController  {
 
-    private final DBConnect dbConnect;
 
-    public IndexController(DBConnect dbConnect){
-        this.dbConnect =dbConnect;
+
+    @FXML
+    public void button_registrer_ny_trening(ActionEvent actionEvent) throws IOException{
+        changeScene();
+    }
+
+
+
+    private void changeScene() throws IOException{
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("form.fxml"));
+        Pane pane = loader.load();
+        FormController controller =  loader.getController();
+        controller.setPrevStage(prevStage);
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        prevStage.close();
+        prevStage.setScene(scene);
+        prevStage.show();
     }
 }
--- src/ux/MainController.java
+++ src/ux/MainController.java
@@ -5,31 +5,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainController  extends Application{
 
-    private final DBConnect dbConnect;
-
-    public MainController(DBConnect dbConnect){
-        this.dbConnect =dbConnect;
-    }
-
+    private static final DBConnect dbConnect = new DBConnectMock();
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader index = new FXMLLoader(MainController.class.getResource("index.fxml"));
-        index.setController(new IndexController(dbConnect));
-        FXMLLoader form = new FXMLLoader(MainController.class.getResource("form.fxml"));
-        form.setController(new FormController(dbConnect));
-        Parent root = index.load();
-        primaryStage.setScene(new Scene(root));
+        FXMLLoader loader = new FXMLLoader(MainController.class.getResource("index.fxml"));
+        Pane pane =  loader.load();
+        IndexController indexController = loader.getController();
+        System.out.println("HEY!");
+        indexController.setPrevStage(primaryStage);
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        DBConnect dbConnect = new DBConnectMock();
-        MainController mainController = new MainController(dbConnect);
-        mainController.launch(args);
     }
 
 }
