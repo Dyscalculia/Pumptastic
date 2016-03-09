3654079891676af9e4a92d85b94480b877aa47b1
haakohu <Haak-hu@hotmail.com>
2016-03-09 18:54:13 +0100

    fixed mock

--- src/Backend/DBConnectMock.java
+++ src/Backend/DBConnectMock.java
@@ -1,5 +1,6 @@
 package backend;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import utils.Group;
@@ -7,28 +8,29 @@ import utils.Workout;
 import utils.Exercise;
 
 public class DBConnectMock implements DBConnect {
+
     @Override
-    public List<Workout> getTreninger() {
+    public List<Workout> getWorkouts() throws SQLException {
         return null;
     }
 
     @Override
-    public Workout getTrening(int id) {
+    public Workout getWorkout(int id) throws SQLException {
         return null;
     }
 
     @Override
-    public void createTrening(Workout trening) {
+    public void createWorkout(Workout workout) {
 
     }
 
     @Override
-    public List<Group> getGroups(Integer parentId) {
+    public List<Group> getGroups(Integer parentId) throws SQLException {
         return null;
     }
 
     @Override
-    public List<Exercise> getExercises(int parentGroupId) {
+    public List<Exercise> getExercises(int parentGroupId) throws SQLException {
         return null;
     }
 }
