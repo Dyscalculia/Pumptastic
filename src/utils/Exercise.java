90b9323da25a6e9d2d79c0f9347cc2a059e1e58b
Stian Hanssen <Hanssen.Stian@gmail.com>
2016-03-13 17:43:08 +0100

    Updated to needs of JavaToSQL

--- src/utils/Exercise.java
+++ src/utils/Exercise.java
@@ -1,8 +1,16 @@
 package utils;
 
-public class Exercise
-{
-
-	public Exercise(int id, String name) {
+public class Exercise {
+	private Integer id;
+	
+	public Exercise(Integer id, String name) {
+	}
+	
+	public Exercise(Integer id, String name, String description, Integer groupId) {
+		
+	}
+	
+	public Integer getId() {
+		return id;
 	}
 }
